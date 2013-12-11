d512fcf197c698585b80fd2ba5155c4af88b6a2a
Gedare Bloom <gedare@rtems.org>
2013-12-11 16:52:10 -0500

    experiments: add algorithms to command line options for run_experiments

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -10,20 +10,25 @@ def usage():
 Usage: python run_experiments.py -[hb:e:]\n\
   -h --help           print this help\n\
   -b --base=          set base directory of dist-rbac-eval [cwd]\n\
-  -e --experiments=   comma-separated list of experiments to run, including\n\
-                      KTZ_inter: KTZ inter-session experiments [default]\n\
+  -e --experiments=   comma-separated list of experiments to run [KTZ_inter]:\n\
+                      KTZ_inter: KTZ inter-session experiments\n\
                       KTZ_intra: KTZ intra-session experiments\n\
+  -a --algorithms=    comma-separated list of algorithms to run [0,1,3]:\n\
+                        0: directed graph\n\
+                        1: access matrix\n\
+                        2: authorization recycling\n\
+                        3: CPOL\n\
+                        4: Bloom and Cascade Bloom filters (unimplemented)\n\
 "
 
-def do_KTZsmall_inter(base):
+def do_KTZsmall_inter(base, algorithms):
   inter = experiment.InterSessionExperiment(base, 'dataset_KTZsubset',
       'stats_KTZsmall')
   sessions = [x for x in range(2, 5)] # 2--4
-  algorithms = [x for x in range(0, 4)] # 0--3
   rh = [ (0, [5]), (1, [1]) ]
   inter.run_experiments(sessions, rh, algorithms)
 
-def do_KTZsmall_intra(base):
+def do_KTZsmall_intra(base, algorithms):
   intra = experiment.IntraSessionExperiment(base, 'dataset_KTZsubset',
       'stats_KTZsmall')
   RP = []
@@ -33,18 +38,16 @@ def do_KTZsmall_intra(base):
   R = 100
   for P in [10,50,100]:
     RP.append((R,P))
-  algorithms = [x for x in range(0, 4)] # 0--3
   intra.run_experiments(RP, algorithms)
 
-def do_KTZsubset_inter(base):
+def do_KTZsubset_inter(base, algorithms):
   inter = experiment.InterSessionExperiment(base, 'dataset_KTZsubset',
       'stats_KTZsubset')
   sessions = [x for x in range(2, 9)] # 2--8
-  algorithms = [x for x in range(0, 4)] # 0--3
   rh = [ (0, [5]), (1, [1]) ]
   inter.run_experiments(sessions, rh, algorithms)
 
-def do_KTZsubset_intra(base):
+def do_KTZsubset_intra(base, algorithms):
   intra = experiment.IntraSessionExperiment(base, 'dataset_KTZsubset',
       'stats_KTZsubset')
   RP = []
@@ -54,17 +57,15 @@ def do_KTZsubset_intra(base):
   R = 100
   for P in [10,50,100,500,700,2000]:
     RP.append((R,P))
-  algorithms = [x for x in range(0, 4)] # 0--3
   intra.run_experiments(RP, algorithms)
 
-def do_KTZ_inter(base):
+def do_KTZ_inter(base, algorithms):
   inter = experiment.InterSessionExperiment(base, 'dataset_KTZ', 'stats_KTZ')
   sessions = [x for x in range(2, 16)] # 2--15
-  algorithms = [x for x in range(0, 4)] # 0--3
   rh = [ (0, [5]), (1, [1]) , (1, [1,2,3,4,5]) ]
   inter.run_experiments(sessions, rh, algorithms)
 
-def do_KTZ_intra(base):
+def do_KTZ_intra(base, algorithms):
   intra = experiment.IntraSessionExperiment(base, 'dataset_KTZ', 'stats_KTZ')
   RP = []
   P = 250
@@ -73,18 +74,18 @@ def do_KTZ_intra(base):
   R = 100
   for P in [10,50,100,500,700,2000,3000,6000,8000,10000]:
     RP.append((R,P))
-  algorithms = [x for x in range(0, 4)] # 0--3
   intra.run_experiments(RP, algorithms)
 
 def main():
   # default args
   base = os.getcwd()
   experiments = ['KTZ_inter']
+  algorithms = ['0', '1', '3']
 
   # Process command line args
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "hb:e:",
-        ["help", "base=", "experiments="]
+    opts, args = getopt.getopt(sys.argv[1:], "hb:e:a:",
+        ["help", "base=", "experiments=", "algorithms="]
     )
   except getopt.GetoptError, err:
     print str(err)
@@ -98,6 +99,8 @@ def main():
       base = arg
     elif opt in ("-e", "--experiments"):
       experiments = arg.split(',')
+    elif opt in ("-a", "--algorithms"):
+      algorithms = arg.split(',')
     else:
       assert False, "unhandled option: " + opt
 
@@ -114,7 +117,7 @@ def main():
       func = dispatch_map[x]
     except KeyError:
       raise ValueError('Invalid experiment ' + x)
-    func(base)
+    func(base, algorithms)
 
 # script entry point
 if __name__ == "__main__":
